452382e166f2a7186ad2938bcd067378ffab53c8
richard-06 <richardroshan0110@gmail.com>
2025-04-01 03:03:20 +0100

    update base contact section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,3 +1,47 @@
+import { Input } from "antd";
+import { useState } from "react";
+
 export function Contact() {
-  return <div> Contact</div>;
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [msg, setMsg] = useState("");
+  return (
+    <div className="flex h-[100vh] items-center justify-center">
+      <div className="rounded-lg border-[0.5px] border-stone-500 py-5 pb-0">
+        <form className="px-5">
+          <div className="text-2xl">Send me a mail.</div>
+
+          <div className="mt-2 w-[300px] text-[12px]">Name</div>
+          <textarea
+            rows={1}
+            value={name}
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 w-full resize-none rounded-md border-[0.5px] border-stone-400 bg-white px-2 py-2 text-sm shadow-[6px_5px_0px_rgba(0,0,0,1)] focus:outline-none"
+          />
+          <div className="mt-2 text-[12px]">Email</div>
+          <textarea
+            placeholder="Enter your mail"
+            rows={1}
+            onChange={(e) => setMail(e.target.value)}
+            value={mail}
+            className="mt-1 w-full resize-none rounded-md border-[0.5px] border-stone-400 bg-white px-2 py-2 text-sm shadow-[6px_5px_0px_rgba(0,0,0,1)] focus:outline-none"
+          />
+          <div className="mt-2 text-[12px]">Your Message</div>
+
+          <textarea
+            onChange={(e) => setMsg(e.target.value)}
+            rows={4}
+            value={msg}
+            placeholder=" Enter your message"
+            className="mt-1 w-full resize-none rounded-md border-[0.5px] border-stone-400 bg-white px-2 py-1 text-sm shadow-[6px_5px_0px_rgba(0,0,0,1)] focus:outline-none"
+          />
+        </form>
+
+        <div className="mx-20 my-4 cursor-pointer rounded-xl bg-black py-2 text-center text-white transition-all duration-300 hover:scale-105">
+          Submit
+        </div>
+      </div>
+    </div>
+  );
 }
